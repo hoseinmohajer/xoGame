525fb44a247d0968c4d20c77d1aeffd5ff9611bd
hoseinmohajer <hosein.mohajer@gmail.com>
2018-05-24 00:15:30 +0430

    set timeout in computer tern mode and add wating dots

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 var pointerFlag = 'O';
-
 var cells = ['11', '12', '13', '21', '22', '23', '31', '32', '33', ]
 var cell = null;
 var xarr = [];
 var oarr = [];
+var winner = false;
 
 function checkWinner() {
 	//rows
@@ -79,83 +79,124 @@ function checkWinner() {
 	}
 }
 function _disable() {
+	winner = true;
 	for (var i = 0; i < cells.length; i++) {
 		var selector = "#" + cells[i];
 		$(selector).attr('onclick','').unbind('click');
 	}
 }
 function _click(cellId) {
-	// remove user clicked cell form array
-	cells.splice(cells.indexOf(cellId),1);
-	selector = $("#" + cellId);
-	if (selector.html() == '&nbsp;') {
-		selector.html(pointerFlag);
-		if(pointerFlag == 'X') {
-			xarr.push(cellId);
-			pointerFlag = 'O';
-			selector.css('color', 'red');
-		} else {
-			oarr.push(cellId);
-			pointerFlag = 'X';
-			selector.css('color', 'blue');
+	if(!winner){
+		// remove user clicked cell form array
+		cells.splice(cells.indexOf(cellId),1);
+		selector = $("#" + cellId);
+		if (selector.html() == '&nbsp;') {
+			selector.html(pointerFlag);
+			if(pointerFlag == 'X') {
+				xarr.push(cellId);
+				pointerFlag = 'O';
+				selector.css('color', 'red');
+			} else {
+				oarr.push(cellId);
+				pointerFlag = 'X';
+				selector.css('color', 'blue');
+			}
+			// select randoum cell from cells array
+			cell = cells[Math.floor(Math.random()*cells.length)];
 		}
-		// select randoum cell from cells array
-		cell = cells[Math.floor(Math.random()*cells.length)];
+		checkWinner();
+	} else {
+		return false;
 	}
-	checkWinner();
 }
 
 $("#11").click(function () {
 	if ( $("#11").html() == '&nbsp;' ) {
 		_click("11");
-		_click(cell);
+		$("#loading").html("...");
+		setTimeout(function () {
+			_click(cell);
+			$("#loading").html("");
+		}, 1000);
 	}
 });
 $("#12").click(function () {
 	if ( $("#12").html() == '&nbsp;' ) {
 		_click("12");
-		_click(cell);
+		$("#loading").html("...");
+		setTimeout(function () {
+			_click(cell);
+			$("#loading").html("");
+		}, 1000);
 	}
 });
 $("#13").click(function () {
 	if ( $("#13").html() == '&nbsp;' ) {
 		_click("13");
-		_click(cell);
+		$("#loading").html("...");
+		setTimeout(function () {
+			_click(cell);
+			$("#loading").html("");
+		}, 1000);
 	}
 });
 $("#21").click(function () {
 	if ( $("#21").html() == '&nbsp;' ) {
 		_click("21");
-		_click(cell);
+		$("#loading").html("...");
+		setTimeout(function () {
+			_click(cell);
+			$("#loading").html("");
+		}, 1000);
 	}
 });
 $("#22").click(function () {
 	if ( $("#22").html() == '&nbsp;' ) {
 		_click("22");
-		_click(cell);
+		$("#loading").html("...");
+		setTimeout(function () {
+			_click(cell);
+			$("#loading").html("");
+		}, 1000);
 	}
 });
 $("#23").click(function () {
 	if ( $("#23").html() == '&nbsp;' ) {
 		_click("23");
-		_click(cell);
+		$("#loading").html("...");
+		setTimeout(function () {
+			_click(cell);
+			$("#loading").html("");
+		}, 1000);
 	}
 });
 $("#31").click(function () {
 	if ( $("#31").html() == '&nbsp;' ) {
 		_click("31");
-		_click(cell);
+		$("#loading").html("...");
+		setTimeout(function () {
+			_click(cell);
+			$("#loading").html("");
+		}, 1000);
 	}
 });
 $("#32").click(function () {
 	if ( $("#32").html() == '&nbsp;' ) {
 		_click("32");
-		_click(cell);
+		$("#loading").html("...");
+		setTimeout(function () {
+			_click(cell);
+			$("#loading").html("");
+		}, 1000);
 	}
 });
 $("#33").click(function () {
 	if ( $("#33").html() == '&nbsp;' ) {
 		_click("33");
-		_click(cell);
+		$("#loading").html("...");
+		setTimeout(function () {
+			_click(cell);
+			$("#loading").html("");
+		}, 1000);
 	}
 });
